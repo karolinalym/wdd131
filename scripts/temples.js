53f32d9deeeab18ccdd17ab167b64e2ac4705e58
karolym1 <karolina@karolinalymphotography.com>
2026-01-15 16:21:03 -0700

    updated

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,10 +2,11 @@
 // Hamburger menu
 // ------------------------------
 const hamButton = document.querySelector("#menu");
-const nav = document.querySelector("nav");
+const navigation = document.querySelector(".navigation");
+
 
 hamButton.addEventListener("click", () => {
-  nav.classList.toggle("open");
+  navigation.classList.toggle("open");
   hamButton.classList.toggle("open");
 });
 
